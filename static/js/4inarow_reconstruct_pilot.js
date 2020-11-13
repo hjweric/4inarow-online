249f3d78b4aac0fd37b42b1b312ec4e47110c5a9
hjweric <zcjtjhu@ucl.ac.uk>
2020-11-12 21:49:06 -0500

    timer issue fixed, waiting for look

--- static/js/4inarow_reconstruct_pilot.js
+++ static/js/4inarow_reconstruct_pilot.js
@@ -48,6 +48,7 @@ function reconstruction_all(game_num){
     load_game_start(game_num)
     mi = 0
     steps = game_data[task_type][game_num-correction_index].length
+    $('#instructions h4').text("you have " + seconds_remain + " seconds left to decide whether this question is true or false:")
     $('.headertext h1').show().text('This sequence has ' + steps.toString() + ' steps').css('color', '#000000');
 
     log_data({"steps":steps, "bs":bp_start,"ws":wp_start})
@@ -266,6 +267,8 @@ function generate_random_tf_euqations(){
 
 
 function distractor_mental_arithmetic(game_num){
+    seconds_remain = 5
+    $('#instructions p').show().text("You have " + seconds_remain + " seconds left to decide whether this question is true or false:");
     display_list =generate_random_tf_euqations()
     true_or_false = Math.round(Math.random())
     equation = display_list[true_or_false]
@@ -274,14 +277,12 @@ function distractor_mental_arithmetic(game_num){
     $('.previousbutton').hide();
     $('#instructions h4').show().text(feedback_text);
     $('#instructions h3').show().text(equation);
-    seconds_remain = 5
-    $('#headertext h1').show().text("you have " + seconds_remain + " seconds left to decide whether this question is true or false:");
 
 
     interval = setInterval(function(){
         seconds_remain--
         $('#instructions h4').hide()
-        $('#instructions p').show().text("You have " + seconds_remain + " seconds left to complete this question");
+        $('#instructions p').show().text("You have " + (seconds_remain).toString() + " seconds left to decide whether this question is true or false:" );
         $('#truebutton').show().off("click").on("click",function(){
             clearInterval(interval)
             if (true_or_false==0){
